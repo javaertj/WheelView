19b1fdce855fb2f301ae2ab69f42cf64bae30101
yankebin <ykbdevelop@gmail.com>
2018-04-11 14:29:41 +0800

    sample代码修改和从jitpack引入WheelView依赖。

--- app/src/main/java/com/ykbjson/app/wheelview/WheelViewAdapter.java
+++ app/src/main/java/com/ykbjson/app/wheelview/WheelViewAdapter.java
@@ -1,6 +1,7 @@
 package com.ykbjson.app.wheelview;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,34 +13,39 @@ import com.ykbjson.lib.wheelview.WheelAdapter;
 import java.util.List;
 
 public class WheelViewAdapter extends WheelAdapter<WheelItem> {
-        private int mSelectPosition;
+    private int mSelectPosition;
 
-        /**
-         * @param context 上下文
-         * @param data    数据源
-         */
-        public WheelViewAdapter(Context context, List<WheelItem> data) {
-            super(context, data, R.layout.include_wheel_view_item);
-        }
-
-        @Override
-        public void onHandleScroll(int selectPosition) {
-            mSelectPosition = selectPosition;
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public void covertView(SimpleAdapterHolder holder, int position, List<WheelItem> dataSource, WheelItem item) {
-            float scale = 1f;
-            if (mSelectPosition == position) {
-                scale = 1.2f;
-            }
-            TextView tvName = holder.getView(R.id.tv_name);
-            tvName.setText(item.getName());
-            ImageView imageView = holder.getView(R.id.iv_avatar);
-            ImageLoader.getInstance().displayImage(item.getAvatarUrl(), imageView);
-
-            ViewHelper.setScaleX(holder.getmConvertView().findViewById(R.id.layout_content), scale);
-            ViewHelper.setScaleY(holder.getmConvertView().findViewById(R.id.layout_content), scale);
-        }
+    /**
+     * @param context 上下文
+     * @param data    数据源
+     */
+    public WheelViewAdapter(Context context, List<WheelItem> data) {
+        super(context, data, R.layout.include_wheel_view_item);
     }
+
+    @Override
+    public void onHandleScroll(int selectPosition) {
+        mSelectPosition = selectPosition;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void covertView(SimpleAdapterHolder holder, int position, List<WheelItem> dataSource, WheelItem item) {
+        float scale = 1f;
+        float alpha = 0.5f;
+        if (mSelectPosition == position) {
+            scale = 1.2f;
+            alpha = 1f;
+        }
+        TextView tvName = holder.getView(R.id.tv_name);
+        ImageView imageView = holder.getView(R.id.iv_avatar);
+        View contentView = holder.getmConvertView().findViewById(R.id.layout_content);
+
+        tvName.setText(item.getName());
+        ImageLoader.getInstance().displayImage(item.getAvatarUrl(), imageView);
+
+        ViewHelper.setScaleX(contentView, scale);
+        ViewHelper.setScaleY(contentView, scale);
+        ViewHelper.setAlpha(contentView, alpha);
+    }
+}
